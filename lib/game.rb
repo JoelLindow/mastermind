664f198364023fda75e89584a1f5db1944b7e3c1
JoelLindow <joellindow@gmail.com>
2017-04-02 23:28:43 -0600

    Add logic for guess comparison

--- lib/game.rb
+++ lib/game.rb
@@ -1,34 +1,75 @@
-class Game
+require './lib/messages'
+require 'pry'
 
-  def main_menu
-    "Welcome to MASTERMIND\n\nWould you like to (p)lay, read the (i)nstructions, or (q)uit?"
+class Game
+  attr_reader :random_combo, :message
+  attr_accessor :current_guess, :guess_checked_for_wrongs, :correct_positions, :correct_colors
+
+  def initialize(random_combo = generate_random_combo)
+    @random_combo = random_combo
+    @message = Messages.new
+    @current_guess = nil
+    @guess_checked_for_wrongs = nil
+    @correct_positions = nil
+    @correct_colors = nil
+  end
+
+  def generate_random_combo
+    pegs = ["R", "G", "B", "Y"]
+    pegs.map do   #<--- iterates through once for each peg in array.
+      pegs.shuffle.pop  #<--- shuffles the array before popping each time map iterates
+    end
+  end
+
+  def start_game
+    take_a_guess
+    @guess_checked_for_wrongs = compare_random_to_guess
+    @correct_positions = count_correct_peg_positions
+    @correct_colors = count_correct_peg_colors
+  end
+
+  def count_correct_peg_positions
+    4 - @guess_checked_for_wrongs.count("WRONG")
+  end
+
+  def count_correct_peg_colors
+    color_check = random_combo.dup
+    current_guess.each do |letter|
+      if color_check.include?(letter)
+        index = color_check.find_index(letter) #<---- this finds the first index where that letter occurs
+        color_check.delete_at(index) #<----- only deletes the element at the index provided in the argumet
+      end
+    end
+    4 - color_check.count
+  end
+
+  def take_a_guess
+      @current_guess = gets.chomp.upcase.chars
+  end
+
+  def compare_random_to_guess
+    random_combo.each_with_index.map do |letter, i|
+      if letter == current_guess[i]
+        letter
+      else
+        "WRONG"
+      end
+    end
+  end
+
+  def show_instructions
+    message.instructions
+  end
+
+  def start_game_message
+    message.start_game_prompt
   end
 
   def quit_game
-    "Goodbye"
+    message.goodbye
   end
 
-  def run(menu_input)
-    commands[menu_input]
-  end
-
-  def commands
-    # { "i" => Instructions.print }
-    { "i" => "These are the instructions",
-      "p" => "This is the game",
-      "q" => "This is how you quit"}
+  def main_menu
+    message.main_menu
   end
 end
-
-#DREAM DRIVEN DEVELOPMENT GOES HERE
-# mastermind = Game.new
-# puts mastermind.main_menu
-# input = gets.chomp
-# puts mastermind.run(input)
-
-
-#write test for run method, while putting in "i" to verify where it takes you
-#write test for quit
-#prite test for play
-
-#While loop (think about it)
